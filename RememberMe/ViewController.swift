e75c8d1531ab4f6e9e3a1212ec7affecc1558ec5
Nicholas Largen <nicholaslargen@nicholass-mbp-3.cuw.edu>
2015-12-01 22:08:46 -0600

    Initial Commit

---
+++ RememberMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RememberMe
+//
+//  Created by Nicholas Largen on 12/1/15.
+//  Copyright © 2015 Nicholas Largen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
